97d4a6dfa4295876f155987fe9beae6b6bce1704
vanvalera <vmarton777@gmail.com>
2024-07-20 16:10:19 +0300

    make build

---
+++ index.08d32ac0.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("main"),t=document.getElementById("card-template");for(let d=0;d<7;d++){let d=t.cloneNode(!0);d.removeAttribute("id"),e.appendChild(d)}
+//# sourceMappingURL=index.08d32ac0.js.map
